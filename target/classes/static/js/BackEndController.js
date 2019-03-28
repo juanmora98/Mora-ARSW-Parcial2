ffe76eb0bfef13cd7b4752072134cfca3190d34e
juanmora98 <juan.mora-q@mail.escuelaing.edu.co>
2019-03-28 18:56:54 -0500

    intento de arreglo en conexion

--- target/classes/static/js/BackEndController.js
+++ target/classes/static/js/BackEndController.js
@@ -1,5 +1,5 @@
 async function getWeather(city){
-    await Promise.resolve(axios.get('https://moraarswt2.herokuapp.com/weather/'+city)
+    await Promise.resolve(axios.getData('https://moraarswt2.herokuapp.com/weather/'+city)
     .then(async function (response){            
         weather.setWeather(response.data);
     }));
